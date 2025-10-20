18d3f0959fd0896c6cd5629b313882d2d31a9e6a
Isaac KM <IsaacKM100@gmail.com>
2025-10-20 16:57:29 -0700

    lab6 code with a bassic test

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
-from routers.items import router as items_router
+from app.routers.items import router as items_router
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/hello")
+def hello():
+    return {"msg": "Hello World"}
 
 app.include_router(items_router)
