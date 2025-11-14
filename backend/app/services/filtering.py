eb583ab4d352f657f2d4aaf1ececad3b4b91b343
Isaac KM <IsaacKM100@gmail.com>
2025-11-14 13:07:21 -0800

    price and category filtering

--- backend/app/services/filtering.py
+++ backend/app/services/filtering.py
@@ -17,15 +17,22 @@ def filter_product_list(target:List[Product],cat_string:str,min_price:int=0,max_
     if cat_string == "all" or cat_string == "":
         cat_filtered = target
     else:
-        #TODO parse category string and apply filter
-        pass
+        categories = cat_string.split("*")
+        for product in target:
+            added = False
+            for category in categories:
+                if(category in product["category"]) and not added:
+                    cat_filtered.append(product)
+                    added = True
     
     result = []
 
     if min_price == 0 and max_price == 0:
         result = cat_filtered
     else:
-        #TODO apply price filtering
-        pass
+        for product in cat_filtered:
+            price = product["discounted_price"]
+            if price <= max_price and price >= min_price:
+                result.append(product)
 
     return result
--- backend/app/services/preview_service.py
+++ backend/app/services/preview_service.py
@@ -1,12 +1,12 @@
 from typing import List
 from app.schemas.product_preview import ProductPreview
+from app.schemas.product import Product
 from app.repositories.products_repo import load_all
 from fastapi import HTTPException
 
-def get_all_product_previews() -> List[ProductPreview]:
-    products = load_all()
+def parse_to_previews(target:List[Product]) -> List[ProductPreview]:
     previews = []
-    for it in products:
+    for it in target:
         arg_list = {"product_id":it["product_id"],
                     "product_name":it["product_name"],
                     "discounted_price":it["discounted_price"],
@@ -14,3 +14,33 @@ def get_all_product_previews() -> List[ProductPreview]:
         previews.append(ProductPreview(**arg_list))
     return previews
 
+def get_all_product_previews() -> List[ProductPreview]:
+    return parse_to_previews(load_all())
+
+from app.services.filtering import filter_product_list
+
+def filter_previews(filter_string:str) -> List[ProductPreview]:
+    #TODO implement a 406 not acceptable error for malformed filter strings
+    category_string = ""
+    parts = filter_string.split("&")
+    category_string = parts[0]
+    filtered_products = []
+    try:
+        if len(parts)>1:
+            max = 0
+            min = 0
+            rating = 0.0
+            if parts[1][:4] == "max=":
+                max = int(parts[1][4:])
+            if len(parts)>2:
+                if parts[2][:4] == "min=":
+                    min = int(parts[2][4:])
+            filtered_products = filter_product_list(load_all(),category_string,min,max)
+
+        else:
+            filtered_products = filter_product_list(load_all(),category_string)
+        return parse_to_previews(filtered_products)
+    except Exception:
+        raise HTTPException(status_code=406,detail="Malformed Filter Request")
+
+    
