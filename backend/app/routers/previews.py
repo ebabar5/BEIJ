a26796e236de609d431ef0de9908b677e57544d4
Isaac KM <IsaacKM100@gmail.com>
2025-11-20 15:31:49 -0800

    basic search functionality, needs refining

--- backend/app/routers/previews.py
+++ backend/app/routers/previews.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter
 from typing import List
 from app.schemas.product_preview import ProductPreview
 from app.services.preview_service import *
@@ -12,3 +12,24 @@ def getPreviews():
 @router.get("/{fil}",response_model=List[ProductPreview])
 def filterPreviews(fil:str=""):
     return filter_previews(fil)
+
+
+#'''Putting this here as for some reason a seperate searchs.py file isn't working
+from app.services.search_service import *
+
+@router.get("/search/",response_model=str)
+def no_search_entry():
+    return "Please enter a search query"
+
+#wide search
+@router.get("/search/w={search_string}",response_model=List[ProductPreview])
+def keywordSearch(search_string:str):
+    keywords = search_string.split(" ")
+    return parse_to_previews(keyword_search(keywords))
+
+#strict search[default]
+@router.get("/search/{search_string}",response_model=List[ProductPreview])
+def keywordSearch(search_string:str):
+    keywords = search_string.split(" ")
+    return parse_to_previews(keyword_search(keywords,strict=True))
+#'''
