9be20a6629b6da626fc9b56c2e2f05f84a83de21
Isaac KM <IsaacKM100@gmail.com>
2025-11-30 19:32:39 -0800

    page heirarchy

--- frontend/app/app/users/page.tsx
+++ frontend/app/app/users/page.tsx
@@ -1,5 +1,21 @@
 import Link from "next/link";
 
 export default function Page(){
-    return <h1>users</h1>
+    return (
+    <div>
+        <h1>What do you wish to do?</h1>
+        <h2>
+            <Link href="users/admin">Admin Login</Link>
+        </h2>
+        <h2>
+            <Link href="users/login">User Login</Link>
+        </h2>
+        <h2>
+            <Link href="users/signup">Register New User</Link>
+        </h2>
+        <h2>
+            <Link href="users/logout">Logout</Link>
+        </h2>
+    </div>
+);
 }
